e64d30f218a07299d6c3dd20b30e16dd37a22336
pronto17 <fenin03@gmail.com>
2022-04-20 04:28:58 +0300

    sorting commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,31 @@ function App()
 {
   return(
     <div className="App">
-<StarRating/>
+      <div className='event-details-container'>
+        <div className='event-details-title'>
+          <table>
+          <tr>
+
+            <td>a<StarRating/></td> 
+            
+          </tr>
+
+          <tr>
+
+            <td>b<StarRating/></td> 
+
+          </tr>
+
+          <tr>
+
+            <td>c<StarRating/></td> 
+
+          </tr>
+
+          </table>
+        </div>
+      </div>
+
     </div>
   )
 }
